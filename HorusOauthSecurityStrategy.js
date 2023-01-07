453474887705f03f71722c3728317713dd4dadf5
Luis Paz Rumiche <lpaz.protiviti@utec.edu.pe>
2023-01-07 00:12:27 -0500

    modificación Horus api~

--- HorusOauthSecurityStrategy.js
+++ HorusOauthSecurityStrategy.js
@@ -139,6 +139,76 @@ function HorusOauthSecurityStrategy(expressServer, options) {
     }
   })
 
+  expressServer.get('/horus/solicitud/login', function (req, res) {
+    if(options.enablePublicLogin === true){
+      logger.info("HorusOauthSecurity solicitud login enabled")
+      var requestId = getRequestId(req);
+
+      var params = {
+        "grantType": options.publicLoginGrantType,
+        "clientId": "",
+        "authorizationCode": "",
+        "genericKey": options.publicLoginGenericKey,
+        "subject": options.publicLoginGenericSubject,
+        "applicationId": options.horusOptions.authenticate.applicationId
+      }
+      horusRestClient.authenticate(params, requestId, function (horusAuthError, horusAuthResponse) {
+        if (horusAuthError) {
+          logger.error("Error in auth transaction: " + horusAuthError);
+          res.redirect(options.express.failureRedirectRoute);
+          return;
+        }
+        if (options.overrideResponse === true && options.defaultBussinessUnit) {
+          logger.info("Modifying default response");
+          var businessUnit = horusAuthResponse.businessUnits.find(bu => bu.identifier === options.defaultBussinessUnit);
+  
+          businessUnit.profiles.forEach(profile => {
+            profile.options = mapMenuReferences(profile.options, options)
+          })
+        } else {
+          logger.info("public login default response will be returned");
+        }
+
+        req.session.tokenInformation = {};
+  
+        req.session.tokenInformation.acquisitionTime = new Date().getTime();
+        req.session.tokenInformation.refreshTokenV1 = horusAuthResponse.refreshTokenV1;
+        req.session.tokenInformation.refreshTokenV2 = horusAuthResponse.refreshTokenV2;
+  
+        //delete unnecesary values
+        delete horusAuthResponse.refreshTokenV1;
+        delete horusAuthResponse.refreshTokenV2;
+  
+        req.session.connectedUserInformation = horusAuthResponse;
+        
+        // injectando valores del public login (tokenV1 y datos básicos)
+        req.session.connectedUserInformation.tokenV1 = req.session.publicUserInformation.tokenV1;
+        req.session.connectedUserInformation.codAlumno = req.session.publicUserInformation.alumno.codPersona;
+        req.session.connectedUserInformation.userName = req.session.publicUserInformation.alumno.userName;
+        req.session.connectedUserInformation.idAlumno = req.session.publicUserInformation.alumno.idAlumno;
+        req.session.connectedUserInformation.documentoIdentidad = req.session.publicUserInformation.alumno.documentoIdentidad;
+        req.session.connectedUserInformation.name = req.session.publicUserInformation.alumno.nomPersona;
+        req.session.connectedUserInformation.mail = req.session.publicUserInformation.alumno.mailRecovery;
+
+        req.session.signinStarted = true;
+        req.session.save();
+  
+        if (req.session.originalUrl) {
+          res.redirect(req.session.originalUrl);
+        } else {
+          res.redirect(options.express.defaultSuccessLoginRoute);
+        }
+  
+        return;
+      });
+
+
+    } else {
+      logger.error("HorusOauthSecurity solicitud login is disabled")
+      res.redirect("/");
+    }
+  })
+
   this.ensureAuthenticated = function (req, res, next) {
 
     logger.debug("ensure if user is authenticated:" + req.path);
